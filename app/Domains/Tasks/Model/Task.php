00d39cd8cb82c7bebe713864cb190841460a0904
EnzoYuri1 <enzobalieiro6@gmail.com>
2026-09-18 09:18:13 -0300

    style: reorder use statements for consistency

--- app/Domains/Tasks/Model/Task.php
+++ app/Domains/Tasks/Model/Task.php
@@ -3,8 +3,8 @@
 namespace App\Domains\Tasks\Model;
 
 use App\Domains\Tasks\Enums\TaskStatus;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Task extends Model
@@ -12,6 +12,7 @@ class Task extends Model
     use HasFactory;
 
     protected $keyType = 'string';
+
     public $incrementing = false;
 
     protected $fillable = [
