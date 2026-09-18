75f3d5bed07496dafbdb5fbf652ef28942952dfe
EnzoYuri1 <enzobalieiro6@gmail.com>
2026-09-18 09:22:25 -0300

    style: reorder comments for clarity and consistency in API routes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Domains\Tasks\Controller\TaskController;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 
-// Rotas Públicas
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+// Rotas Públicas: Acessíveis sem autenticação
 Route::post('login', [AuthController::class, 'login']);
 
-// Rotas Protegidas
+// Rotas Protegidas: Requerem token de acesso via Sanctum
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
+    // Gerenciamento de Tarefas
     Route::apiResource('tasks', TaskController::class);
     Route::patch('tasks/{id}/complete', [TaskController::class, 'complete']);
 });
